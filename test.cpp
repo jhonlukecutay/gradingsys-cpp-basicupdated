481e51eda0fac49f1f5e867b5140085141f15a27
Jhon Luke Cutay <113082100+jhonlukecutay@users.noreply.github.com>
2022-09-24 20:50:52 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<numeric>
+using namespace std;
+int grade[5],x;
+int totalg, average;
+
+main()
+{
+	for(x=0;x<=4;x++)
+	{
+		cout<<"Enter Grade:";
+		cin>>grade[x];
+	}
+		average = totalg / 5;
+		cout<<"Total grade is: "<< average;
+
+}
